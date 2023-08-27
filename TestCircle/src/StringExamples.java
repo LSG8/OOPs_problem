0d4de3a425612e332aa193a8782b08958a30cb72
Lahari Sengupta <jhinik8@gmail.com>
2023-08-27 11:01:50 +0300

    del

--- TestCircle/src/StringExamples.java
+++
@@ -1,6 +0,0 @@
-public class StringExamples {
-    public static void main() {
-        String str1 = new String("This is my first string");//object name = str1
-        System.out.println(str1);
-    }
-}
